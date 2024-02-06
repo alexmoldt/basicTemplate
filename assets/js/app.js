936d864bbd816b48d43e28ee4d878205b3b1ed11
alexmoldt <alexmoldt@outlook.com>
2024-02-06 10:17:01 +0100

    Update app.js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+let myAge = 29;
+let myName = "Alex";
+let myShoesize = 45;
+
+console.log("myAge er " + myAge);
+console.log("Myname er: " + myName);
+console.log("My shoesize er: " + myShoesize);
+
+console.log("Hejsa + ", myName);
